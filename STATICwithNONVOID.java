2e1504e8ef13424cace4931a450aa5aed17e2d99
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-11 11:29:16 +0530

    Add files via upload

---
+++ STATICwithNONVOID.java
@@ -0,0 +1,13 @@
+class STATICwithNONVOID
+{
+    public static String s()
+    {
+        return "everyone come plz";
+
+    }
+    public static void main(String args[])
+    {
+    String res=s();
+    System.out.println(res);
+    }
+}
