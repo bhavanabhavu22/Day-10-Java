2fe47c4adcea1910b6e837f70e6cf7b9bb688dcf
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-11 21:26:09 +0530

    Add files via upload

---
+++ THISkeyword.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class THISkeyword {
+    void display()
+    {
+        System.out.println("hey jenne");
+    }
+    void dis1()
+    {
+        this.display();
+    }
+    public static void main(String args[])
+    {
+        THISkeyword b=new THISkeyword();
+        b.display();
+    }
+}
