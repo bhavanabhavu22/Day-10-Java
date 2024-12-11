b91df4e2bcf83b7a05066a803c9a6146c2c7bad3
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-11 11:29:01 +0530

    Add files via upload

---
+++ PROTECTED.java
@@ -0,0 +1,10 @@
+public class PROTECTED {
+    public static void s()
+    {
+        System.out.println("hey guys welcome");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
