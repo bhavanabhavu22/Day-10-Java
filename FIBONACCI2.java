66645f34dcb7db4dcbc2c8659408c4c252c10533
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-11 21:38:58 +0530

    Add files via upload

---
+++ FIBONACCI2.java
@@ -0,0 +1,14 @@
+class FIBONACCI2 {
+    public static void main(String args[])
+    {
+        int a=0;
+        int b=1;
+        for(int c=2;c<=10;c++);
+        {
+            int i=a+b;
+            System.out.println(c+"->"+c);
+            a=b;
+            b=c;
+        }
+    }
+}
